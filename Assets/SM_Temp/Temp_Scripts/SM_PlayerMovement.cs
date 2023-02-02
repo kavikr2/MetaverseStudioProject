cc291409496066908472bb221c16f5ef692dadd6
kavikr2 <kavikr2@gmail.com>
2023-02-02 14:35:43 +0800

    Temp Folder Commit

---
+++ Assets/SM_Temp/Temp_Scripts/SM_PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SM_PlayerMovement : MonoBehaviour
+{
+    float speed = 0f;
+    Animator animator;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Animations
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            animator.SetBool("isWaving", true);
+        }
+        else
+        {
+            animator.SetBool("isWaving", false);
+
+        }
+
+
+        //Movement 
+        if (speed > 0) { speed -= 0.1f; }
+        if (speed < 0) { speed += 0.1f; }
+        if (Input.GetKey(KeyCode.W) && speed < 4) 
+        {
+            animator.SetBool("isWalking", true);
+            speed += 0.5f; 
+        }
+        if (Input.GetKey(KeyCode.S) && speed < -2) { speed -= 0.5f; }
+
+        if (Input.GetKey(KeyCode.A)) { transform.Rotate(0, -2, 0); }
+        if (Input.GetKey(KeyCode.D)) { transform.Rotate(0, 2, 0); }
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+
+    }
+
+    
+}
