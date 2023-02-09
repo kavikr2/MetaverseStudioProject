ee1b10c0558fc086868554be833ec06bce83e39c
kavikr2 <kavikr2@gmail.com>
2023-02-09 12:57:51 +0800

    GameManager Script

---
+++ Assets/SM_Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this; 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
