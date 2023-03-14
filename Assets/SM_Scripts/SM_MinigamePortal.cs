41ad590e60375120a3f0625a15c848157b2c73a3
DragneelDM <64640316+DragneelDM@users.noreply.github.com>
2023-03-14 10:27:26 +0800

    Quick Minigame bug fix

--- Assets/SM_Scripts/SM_MinigamePortal.cs
+++ Assets/SM_Scripts/SM_MinigamePortal.cs
@@ -1,13 +1,17 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class SM_MinigamePortal : MonoBehaviour
 {
     public SM_MinigameManager manager;
     public Minigames game;
+    PhotonView _view;
 
     public void OnCollisionEnter(Collision collision)
     {
-        manager.EnterMiniGame((int)game);
+        _view = GetComponent<PhotonView>();
+        if(_view.IsMine)
+            manager.EnterMiniGame((int)game);
     }
 
 }
